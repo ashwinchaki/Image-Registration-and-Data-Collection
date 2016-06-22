a88a17f3a25b72f0d5b22e69bd1bdf7339bee1af
Ashwin Chakicherla <darthmarth550@gmail.com>
2016-06-22 11:10:39 -0700

    Updated formatCSV

--- MatLab Scripts/formatCSV.m
+++ MatLab Scripts/formatCSV.m
@@ -1,15 +1,26 @@
-function formatCSV(mousename)
+workingarr = csvread('Transformation_Points.csv');
+delete('Transformation_Points.csv');
+transformpoints = struct;
+d=dir('*.png'); % <- retrieve all names: file(s) and folder(s)
+d=d(~[d.isdir]); % <- keep file name(s), only
+d={d.name}.'; % <- file name(s)
+nf=numel(d);
+for a=2:nf
 {
-    workingarr = csvread('Transformation_Points.csv');
-    delete('Transformation_Points.csv');
-    numberOfFiles = size(workingarr,2)/3;
-    (mousename) = struct;
-    inputNumber = size(images);
-    d=dir('*.png'); % <- retrieve all names: file(s) and folder(s)
-    d=d(~[d.isdir]); % <- keep file name(s), only
-    d={d.name}.'; % <- file name(s)
-    nf=numel(d);
-    for i=2:nf
+    currentfile = d(a);
+    for b = 1:3
     {
-        currentfile = d{i};
+        newarray(1,:) = workingarray(i,:);
+        newarray(2,:) = workingarray(i+1,:);
+        newarray(3,:) = workingarray(i+2,:);
+    }
+
+    transformpoints.(currentfile) = newarray;
+    end
+end
+delete('Transformation_Points.csv');
+save(mousename,transformationpoints);
+
+
+        
         
